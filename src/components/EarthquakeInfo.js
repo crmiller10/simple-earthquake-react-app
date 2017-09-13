753670d4c89f1c3f9f3502ba89cf16caa3d3a094
Collin Miller <crmiller10@gmail.com>
2017-09-13 15:52:31 -0400

    Initial Commit

---
+++ src/components/EarthquakeInfo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Moment from 'moment';
+import earthquakes from "../data/earthquakes.js"
+import '../styles/App.css';
+export default class EarthquakeInfo extends Component {
+  render () {
+    return (
+      <div className="earthquake-title">
+        This is a list of 8 Earthquakes occurring on the morning of July 14th across the United States.
+      </div>
+    )
+  }
+}
